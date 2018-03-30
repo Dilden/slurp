cea8b40fa740804e72da184345dbad291964d65d
Dylan <dylan.hildenbrand@gmail.com>
2018-03-29 22:38:04 -0500

    script will download a web page

--- classes/Slurp.php
+++ classes/Slurp.php
@@ -2,16 +2,36 @@
 
 namespace slurp;
 
+use GuzzleHttp\Client;
+
 class Slurp {
 
-	function __construct($config) {
+	// bring user config options + CLI parameters
+	function __construct($config, $params) {
 		$this->config = $config;
+		$this->params = $params;
 	}
 
+	// main functionality
 	public function run() {
-		foreach($this->config as $conf) {
-			echo $conf . "\n";
+
+		if(!isset($this->params[1])) {
+			return "Missing URL parameter \n";
 		}
-		return "huzzah \n";
+
+		$url = $this->params[1];
+		$directory = getcwd(). "/" . $this->config->directory . "/";
+
+		$client = new client([
+			'base_uri' => $url,
+			'timeout' => 2
+		]);
+
+		$response = $client->request('GET');
+		$body = $response->getBody();
+
+		file_put_contents($directory . parse_url($url,  PHP_URL_HOST) . ".html", (string) $body);
+
+		return "huzzah! \n";
 	}
 }
